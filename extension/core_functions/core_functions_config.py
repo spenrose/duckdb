a28ec007a5636ba4e1469f4b61c1cd673b8fa820
Scott Penrose <penrose@gmail.com>
2024-11-04 13:18:07 -0500

    try a fix for python 3.10 code

--- extension/core_functions/core_functions_config.py
+++ extension/core_functions/core_functions_config.py
@@ -3,4 +3,18 @@ import glob, os
 prefix = os.path.join('extension', 'core_functions')
 
 include_directories = [os.path.join(prefix, x) for x in ['include']]
-source_files = [os.path.join(prefix, x) for x in glob.glob("**/*.cpp", root_dir=prefix, recursive=True)]
+
+# Save the current working directory
+current_dir = os.getcwd()
+
+try:
+    # Change to the target directory
+    os.chdir(prefix)
+    
+    # Use glob to find the files, without root_dir
+    source_files = [os.path.join(prefix, x) for x in glob.glob("**/*.cpp", recursive=True)]
+
+finally:
+    # Restore the original working directory
+    os.chdir(current_dir)
+
